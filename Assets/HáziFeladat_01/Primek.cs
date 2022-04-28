dae9bfe0a578c7a61a433d390b06b4b07f6c643f
DJozsef86 <104295287+DJozsef86@users.noreply.github.com>
2022-04-28 21:21:35 +0200

    Prim

---
+++ Assets/HáziFeladat_01/Primek.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Primek : MonoBehaviour
+
+{
+    void Start()
+    {
+
+        for (int i = 1; i <= 100; i++)
+        {
+            while
+        }
+
+    }
+
+}
